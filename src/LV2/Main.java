edc02efee3a3ef5a10e844819bd650b57c894d8d
Che0807 <elpin0428@naver.com>
2025-04-25 20:28:32 +0900

    fix Main while 문 수정

--- src/LV2/Main.java
+++ src/LV2/Main.java
@@ -12,6 +12,20 @@ public class Main {
         menu.addMenuItem(new MenuItem(3, "Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
         menu.addMenuItem(new MenuItem(4, "Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
 
+        boolean isRunning = true;
+
+        while (isRunning) {
+            menu.printMenu();
+            System.out.println("0. 종료 | 종료");
+            System.out.print("원하는 메뉴 번호를 입력하세요: ");
+            int order = scanner.nextInt();
+
+            if (order == 0) {
+                System.out.println("프로그램을 종료합니다.");
+                isRunning = false;
+            }
+        }
+
         while (true) {
             // 메뉴 출력
             menu.printMenu();
