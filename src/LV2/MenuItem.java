09704209bbb8f593dfafe510ef9bad5fec40b574
Che0807 <elpin0428@naver.com>
2025-04-25 15:42:10 +0900

    LV2 MenuItem 클래스 생성

--- src/LV2/MenuItem.java
+++ src/LV2/MenuItem.java
@@ -1,4 +1,15 @@
 package LV2;
 
 public class MenuItem {
+    String name;         // 햄버거 이름
+    int price;           // 햄버거 가격
+    String description;  // 햄버거 설명
+
+    // 생성자 - MenuItem 객체 만들 때 초기값을 설정
+    public MenuItem(String name, int price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
 }
+
