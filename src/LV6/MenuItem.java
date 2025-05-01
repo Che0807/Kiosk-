b03ee47532a803e052a7fbbe440bf29653089a61
Che0807 <elpin0428@naver.com>
2025-05-01 10:36:52 +0900

    LV6 시작 MenuItem 객체 생성 MenuType enum 생성

---
+++ src/LV6/MenuItem.java
@@ -0,0 +1,27 @@
+package LV6;
+
+public class MenuItem {
+    private String name;         // 이름
+    private double price;        // 가격
+    private String description;  // 설명
+
+    // 생성자
+    public MenuItem(String name, double price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    // getter 메서드들
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
---
+++ src/LV6/MenuType.java
@@ -0,0 +1,8 @@
+package LV6;
+
+public enum MenuType {
+    BURGER,
+    DRINK,
+    DESSERT
+}
+
